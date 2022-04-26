798cd5112a66aa2141190df89af10b4acd0772c6
Jakub Sosnowski <jakub.diversa@wp.pl>
2022-04-26 15:26:51 +0200

    Added random friends by fetch API

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -1,6 +1,53 @@
+import { useState } from "react";
+
+import FriendsList from "./FriendsList";
+
+import styles from './Friends.module.css'
+
 const Friends = () => {
+
+  const [listOfFriends, setlistOfFriends] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const API = 'https://randomuser.me/api/?results=3';
+
+  const getRandomUsers = () => {
+  
+    fetch(API)
+    .then(response => {
+      if(response.ok) {
+        return response.json()
+      }
+      throw response
+    })
+    .then(data => {
+      setlistOfFriends(data.results);
+    })
+    .catch(error => {
+      console.error('Error fetching data', error);
+      setError(error);
+    })
+    .finally(() => {
+      setLoading(false);
+    })
+
+    if (loading) return 'Loading...';
+    if (error) return 'Error!';
+  }
+
+
   return (  
-    <p>Taka galeria przyjaciół z apki</p>
+    <div className={styles.container}>
+      <h1>Osoby dodane przez Ciebie jako znajomi</h1>
+      <div>
+        <ul>
+          {listOfFriends && <FriendsList listOfFriends={listOfFriends} />}
+        </ul>
+      </div>
+      <button onClick={getRandomUsers}>Osoby, które możesz znać</button>
+    </div>
+
   );
 }
  
---
+++ src/components/FriendsList.jsx
@@ -0,0 +1,18 @@
+import styles from './FriendsList.module.css'
+
+const FriendsList = ({listOfFriends}) => {
+
+  const friend = listOfFriends.map(data => (
+    <div className={styles.friend} key={data.login.uuid}>
+      <img className={styles.img} src={data.picture.medium} alt={data.name.last}/>
+      <p>{data.name.first} {data.name.last}</p>
+    </div>
+  ))
+  return (  
+    <div>
+      {friend}
+    </div>
+  );
+}
+ 
+export default FriendsList;
